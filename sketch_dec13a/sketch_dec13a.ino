11f3422515bf4d4d50b90bf36b17ff6b5107f5f2
ehsansiddiqui <ihsaansiddiqui@gmail.com>
2021-12-13 12:41:40 +0100

    Fifth commit for code change

--- sketch_dec13a/sketch_dec13a.ino
+++ sketch_dec13a/sketch_dec13a.ino
@@ -18,19 +18,7 @@ void setup()
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
   int i;
-  //sensor calibration
-  for(i = 0; i < 80; i++)
-  {
-    if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
-      motors.setSpeeds(-200, 200);
-    else
-      motors.setSpeeds(200, -200);
-    reflectanceSensors.calibrate();
-
-    // Since our counter runs to 80, the total delay will be
-    // 80*20 = 1600 ms.
-    delay(20);
-  }
+  reflectanceSensors.calibrate(); //sensor calibration
   motors.setSpeeds(0,0); // init speed(left,right)->speed(0,0)
   digitalWrite(13, LOW); // calibrate and speed initialization finished
   button.waitForButton();
